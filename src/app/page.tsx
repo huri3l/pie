e75705ea9cb4c3b937a76b87dd7366a7fab65459
Huriel Ferreira Lopes <huriel.lopes@compasso.com.br>
2024-04-17 16:15:52 -0300

    scaffold

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/cacf7ae6-e1fc-448a-93dd-703e65570853-ja6c04.com_berserk-guts-drawing-22_1920x1080.jpg",
+  "https://utfs.io/f/470b2572-b2f8-4c45-84d8-fd86c40b2809-4kj4dg.png",
+  "https://utfs.io/f/c6ad6f33-0a55-4ff3-a69b-59040d76f91d-aowelu.jpeg",
+  "https://utfs.io/f/a05ef311-ecdd-4a32-a9da-b608d9bbe58e-v3bk4i.png",
+  "https://utfs.io/f/0ce796cf-c50f-4724-b650-e6dcbd91155a-xv5sev.png",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (gallery in progress)
+    <main>
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
