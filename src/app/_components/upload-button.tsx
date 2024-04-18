49af4d77668b0708f3c59c7e0c9452a34dbb6503
Huriel Ferreira Lopes <huriel.lopes@compasso.com.br>
2024-04-18 17:50:10 -0300

    add sonner

--- src/app/_components/upload-button.tsx
+++ src/app/_components/upload-button.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 import { useUploadThing } from "~/utils/uploadthing";
 
 type Input = Parameters<typeof useUploadThing>;
@@ -50,7 +51,15 @@ function UploadIcon() {
 export function SimpleUploadButton() {
   const router = useRouter();
   const { inputProps } = useUploadThingInputProps("imageUploader", {
+    onUploadBegin() {
+      toast("Uploading...", {
+        duration: 100000,
+        id: "upload-begin",
+      });
+    },
     onClientUploadComplete() {
+      toast.dismiss("upload-begin");
+      toast("Upload complete!");
       router.refresh();
     },
   });
