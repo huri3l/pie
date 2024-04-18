abb8ad21d718dc67f9b6fd400c12f5814a547e70
Huriel Ferreira Lopes <huriel.lopes@compasso.com.br>
2024-04-18 18:26:32 -0300

    add styling and loading spinner

--- src/app/_components/upload-button.tsx
+++ src/app/_components/upload-button.tsx
@@ -48,14 +48,44 @@ function UploadIcon() {
   );
 }
 
+function LoadingSpinner() {
+  return (
+    <svg
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="white"
+    >
+      <style>{"@keyframes spinner_AtaB{to{transform:rotate(360deg)}}"}</style>
+      <path
+        d="M12 1a11 11 0 1011 11A11 11 0 0012 1zm0 19a8 8 0 118-8 8 8 0 01-8 8z"
+        opacity={0.25}
+      />
+      <path
+        d="M10.14 1.16a11 11 0 00-9 8.92A1.59 1.59 0 002.46 12a1.52 1.52 0 001.65-1.3 8 8 0 016.66-6.61A1.42 1.42 0 0012 2.69a1.57 1.57 0 00-1.86-1.53z"
+        style={{
+          transformOrigin: "center",
+          animation: "spinner_AtaB .75s infinite linear",
+        }}
+      />
+    </svg>
+  );
+}
+
 export function SimpleUploadButton() {
   const router = useRouter();
   const { inputProps } = useUploadThingInputProps("imageUploader", {
     onUploadBegin() {
-      toast("Uploading...", {
-        duration: 100000,
-        id: "upload-begin",
-      });
+      toast(
+        <div className="flex items-center gap-2 text-white">
+          <LoadingSpinner /> Uploading...
+        </div>,
+        {
+          duration: 100000,
+          id: "upload-begin",
+        },
+      );
     },
     onClientUploadComplete() {
       toast.dismiss("upload-begin");
