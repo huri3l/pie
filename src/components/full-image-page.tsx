7afcbaad2cf420018230aa600a07db15c4e13cbe
Huriel Ferreira Lopes <huriel.lopes@compasso.com.br>
2024-04-18 16:48:38 -0300

    modal adjustments

---
+++ src/components/full-image-page.tsx
@@ -0,0 +1,20 @@
+import { getImage } from "~/server/queries";
+
+export default async function FullPageImageView(props: { id: number }) {
+  const idAsNumber = Number(props.id);
+  if (Number.isNaN(idAsNumber)) throw new Error("Invalid photo id");
+
+  const image = await getImage(idAsNumber);
+
+  return (
+    <div className="flex h-full w-full min-w-0">
+      <div className="flex flex-shrink items-center justify-center">
+        <img src={image.url} className="flex-shrink object-contain" />
+      </div>
+
+      <div className="flex w-48 flex-shrink-0 flex-col border-l">
+        <div className="text-xl font-bold">{image.name}</div>
+      </div>
+    </div>
+  );
+}
